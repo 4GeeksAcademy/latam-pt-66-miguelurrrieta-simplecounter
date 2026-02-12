c40c6a896e30ccd33f98829486d6a73ad1fdf688
miguelurrieta166 <miguelurrieta166@gmail.com>
2026-02-12 01:58:56 +0000

    cambios

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,31 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
+import "../styles/index.css"; 
 
-function ContadorSimple() {
-    const [seconds, setSeconds] = useState(0);
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setSeconds(prev => prev + 1);
-        }, 1000);
-        return () => clearInterval(interval);
-    }, []);
-
-    const four = Math.floor(seconds / 1000) % 10;
-    const three = Math.floor(seconds / 100) % 10;
-    const two = Math.floor(seconds / 10) % 10;
-    const one = seconds % 10;
-
+function ContadorSimple(props) {
     return (
         <div className='bigCounter'>
             <div className='calendar'>
                 <i className="fa-regular fa-clock"></i>
             </div>
-            <div className='four'>{four}</div>
-            <div className='three'>{three}</div>
-            <div className='two'>{two}</div>
-            <div className='one'>{one}</div>
+            <div className='four'>{props.digitFour % 10}</div>
+            <div className='three'>{props.digitThree % 10}</div>
+            <div className='two'>{props.digitTwo % 10}</div>
+            <div className='one'>{props.digitOne % 10}</div>
         </div>
     );
 }
 
-export default ContadorSimple;
+let counter = 0;
+setInterval(function() {
+    const four = Math.floor(counter / 1000)%10;
+    const three = Math.floor(counter / 100)%10;
+    const two = Math.floor(counter / 10)%10;
+    const one = Math.floor(counter / 1)%10;
+   
+    counter++;
+
+    
+    ReactDOM.render(
+        <ContadorSimple
+            digitOne={one}
+            digitTwo={two}
+            digitThree={three}
+            digitFour={four}
+        />,
+        document.querySelector("#root") 
+    );
+}, 1000);
+
+ 
